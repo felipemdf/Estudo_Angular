04fcb4aa205ef42546e5e89e6f010b44c3393296
Felipe Marinho de Freitas <f.marinho162001@gmail.com>
2022-08-10 23:29:35 -0300

    adiciona diretivas

--- Projeto/src/app/app.module.ts
+++ Projeto/src/app/app.module.ts
@@ -3,15 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
+<<<<<<< HEAD
 import { ParentDataComponent } from './components/parent-data/parent-data.component';
 import { DirectivesComponent } from './components/directives/directives.component';
+=======
+>>>>>>> parent of 4ed8a58 (adiciona a passagem de dados entre componentes)
 
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< HEAD
     FirstComponentComponent,
     ParentDataComponent,
     DirectivesComponent
+=======
+    FirstComponentComponent
+>>>>>>> parent of 4ed8a58 (adiciona a passagem de dados entre componentes)
   ],
   imports: [
     BrowserModule
