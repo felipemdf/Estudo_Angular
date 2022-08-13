3ede590448183278f8617570addaa4bb673b9949
Felipe Marinho de Freitas <f.marinho162001@gmail.com>
2022-08-13 16:39:40 -0300

    adiciona exemplo de two way bind

--- Projeto/src/app/app.module.ts
+++ Projeto/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { DirectivesComponent } from './components/directives/directives.component';
 import { EventosComponent } from './components/eventos/eventos.component';
 import { ListRenderComponent } from './components/list-render/list-render.component';
 import { PipesComponent } from './components/pipes/pipes.component';
+import { TwoWayBindComponent } from './components/two-way-bind/two-way-bind.component';
 
 
 @NgModule({
@@ -16,9 +19,11 @@ import { PipesComponent } from './components/pipes/pipes.component';
     EventosComponent,
     ListRenderComponent,
     PipesComponent,
+    TwoWayBindComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
